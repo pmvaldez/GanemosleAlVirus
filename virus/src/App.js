97668cf8fcbd224d08fe7bfb8fc01a3cc6b3265b
pmvaldez <valdezp69@gmail.com>
2020-08-24 19:54:32 -0400

    Navbar ppal

--- virus/src/App.js
+++ virus/src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import Game from './components/Game';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-     <Game />
+    <Router>
+    <div>
+        <Switch>
+            <Route path="/Game">
+              <Game />
+            </Route>
+            <Route path="/admin">
+                Ruta de administracion
+            </Route>
+            <Route path="/" exact>
+                <Home />
+            </Route>
+        </Switch>
     </div>
+</Router>    
   );
 }
 
---
+++ virus/src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {Link, NavLink} from 'react-router-dom'
+import logo from '../img/logotipo.jpg'
+import '../styles/navbar.css';
+
+const Navbar = () => {
+    return (
+        <div className="navbar ">
+        <Link to="/" className="navbar-brand"> <img src={logo} width="70" height="30" alt=""/></Link>
+        <div>
+            <div className="d-flex">
+                <NavLink 
+                    className="btn btn-nav mr-2" 
+                    to="/"
+                    exact
+                >
+                    Inicio
+                </NavLink>
+                <NavLink 
+                    className="btn btn-nav mr-2" 
+                    to="/admin"
+                >
+                    Admin
+                </NavLink>
+                <NavLink 
+                    className="btn btn-nav" 
+                    to="/login"
+                >
+                    Login
+                </NavLink>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default Navbar
