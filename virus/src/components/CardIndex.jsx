6e3eb04cb91d46fa4380987175106ea999ddbfc0
GenesisAstete <genesis.astete.a@gmail.com>
2020-08-26 22:09:37 -0400

    home gav

--- virus/src/components/CardIndex.jsx
+++ virus/src/components/CardIndex.jsx
@@ -1,21 +1,26 @@
 import React from "react";
-import { MDBCard, MDBCardTitle, MDBCardImage, MDBCardText, MDBCardBody } from "mdbreact";
+import '../styles/cardindex.css'
+
 import hootie from '../img/hootie.png'
 
 const CardIndex = () => {
   return (
-      <MDBCard style={{ maxWidth: "22rem", text: 'center', display: 'flex', alignItems: 'center', justifyContent: 'center', }}>
-        <MDBCardImage src={hootie} alt="MDBCard image cap" top hover
-          overlay="white-slight" />
-        <MDBCardBody>
-          <MDBCardTitle tag="h5">¿Que es?</MDBCardTitle>
-          <MDBCardText>
-          Ganémosle al Virus es una plataforma informativa y educativa que funciona como herramienta para que padres 
+
+
+  <div className="d-flex flex-column justify-content-center align-self-center p-5">
+   
+    <div className="mx-auto p-4">
+    <img src={hootie} alt="Responsiveimg" width='250'/>
+    </div>
+
+    <div className="mx-auto">
+   <p className="card-text">Ganémosle al Virus es una plataforma informativa y educativa que funciona como herramienta para que padres 
           y tutores puedan enseñar a los niños bajo su cuidado a entender qué está sucediendo en la actualidad en el 
-          contexto de la pandemia del COVID-19 y dar técnicas de autocuidado para poder hacerle frente al virus.
-          </MDBCardText>
-        </MDBCardBody>
-      </MDBCard>
+          contexto de la pandemia del COVID-19 y dar técnicas de autocuidado para poder hacerle frente al virus.</p>
+    </div>
+    
+  </div>
+
   );
 }
 
