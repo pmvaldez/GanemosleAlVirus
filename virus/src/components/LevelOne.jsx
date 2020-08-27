3f7d1cd261019c9d54e34ae50d2325586a4881fd
Rosario <rosario.wrobel@gmail.com>
2020-08-27 19:21:24 -0400

    levelone

--- virus/src/components/LevelOne.jsx
+++ virus/src/components/LevelOne.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import '../styles/levelOne.css';
 import { Link } from "react-router-dom";
+import Navbar from './Navbar'
 
 const levelOne = () => {
     return (
+<div><Navbar/>        
     <div className="sectionGAV">
+        
         
              <h1>Etapa 1: ¿Que es el Covid-19?</h1>     
         
-    <div className="containerAllBtn">
-    <Link  to="/levelTwo" className="link">
+        <div className="containerAllBtn">
+            <Link  to="/levelTwo" className="link">
         <button className="btnTwo">
             <h4 className="tittleBtn">Etapa 2</h4>                            
         </button>
-    </Link>
-</div>  
-</div>   
+            </Link>
+        </div>  
+    </div>   
+</div>
     )
 }
 
