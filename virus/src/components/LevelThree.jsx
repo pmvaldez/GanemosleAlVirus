b76070b015c1ac6f4a0fafbdf6bf1c68e7e195ad
Rosario <rosario.wrobel@gmail.com>
2020-08-27 21:58:52 -0400

    vistas

--- virus/src/components/LevelThree.jsx
+++ virus/src/components/LevelThree.jsx
@@ -1,22 +1,26 @@
-import React from 'react';
-import '../styles/levelTwo.css';
-import { Link } from "react-router-dom";
+import React from "react";
+import "../styles/levelOne.css";
+//import { Link } from "react-router-dom";
+//import ProgressBar from './ProgressBar'
+//import Footer from './Footer'
 
-const levelThree = () => {
-    return (
-        <div className="sectionLevelthree">
-             <h1>Etapa 3</h1>  
-             <div className="containerAllBtn">
-                    <Link to="/levelOne" className="link" >
-                        <button className="btnOne">
-                            <h4 className="tittleBtn">Etapa 1</h4>                            
-                        </button>
-                    </Link>
-                </div>  
-
-        </div> 
-    )
-}
-
-export default levelThree
+const levelOne = () => {
+  return (
+    <div className="d-flex">
+      <div className="section-mod1-general">
+        {/* <h1>Etapa 1: ¿Que es el Covid-19?</h1> */}
+        {/* <div className="containerAllBtn">
+          <Link to="/levelTwo" className="link">
+            <button className="btnTwo">
+              <h4 className="tittleBtn">Etapa 2</h4>
+            </button>
+          </Link>
+        </div> */}
+{/*<ProgressBar/>*/}
+      </div>
+      {/* <Footer/> */}
+    </div>
+  );
+};
+export default levelOne;
 
--- virus/src/components/LevelTwo.jsx
+++ virus/src/components/LevelTwo.jsx
@@ -1,21 +1,26 @@
-import React from 'react';
-import '../styles/levelTwo.css';
-import { Link } from "react-router-dom";
+import React from "react";
+import "../styles/levelOne.css";
+//import { Link } from "react-router-dom";
+//import ProgressBar from './ProgressBar'
+//import Footer from './Footer'
 
-const levelTwo = () => {
-    return (
-        <div className="sectionLeveltwo">
-             <h1>Etapa 2: Cuidados</h1>  
-            <div className="containerAllBtn">
-                    <Link to="/levelThree" className="link">
-                        <button className="btnThree">
-                            <h4 className="tittleBtn">Etapa 3</h4>                            
-                        </button>
-                    </Link>
-            </div>   
-        </div> 
-    )
-}
-
-export default levelTwo
+const levelOne = () => {
+  return (
+    <div className="d-flex">
+      <div className="section-mod1-general">
+        {/* <h1>Etapa 1: ¿Que es el Covid-19?</h1> */}
+        {/* <div className="containerAllBtn">
+          <Link to="/levelTwo" className="link">
+            <button className="btnTwo">
+              <h4 className="tittleBtn">Etapa 2</h4>
+            </button>
+          </Link>
+        </div> */}
+{/*<ProgressBar/>*/}
+      </div>
+      {/* <Footer/> */}
+    </div>
+  );
+};
+export default levelOne;
 
