a8ec746aa6c604303a5a2dbb56eb1a46e2689058
ivvnv <ivana.rivera.c@gmail.com>
2020-09-02 19:44:41 -0400

    relleno de zona de tutores

--- virus/src/components/ZonaTutores.jsx
+++ virus/src/components/ZonaTutores.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import "../styles/zonatutores.css";
 import NavbarPage from "./NavbarPage";
+import Footer from './Footer'
 
 const ParaTutores = () => {
   return (
@@ -15,9 +16,11 @@ const ParaTutores = () => {
     <div className="episode__content">
       <div className="title">Inicio</div>
       <div className="story">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non ipsum ullamcorper, ullamcorper ex.</p>
-        <p>Aenean ut nibh ac metus fringilla lacinia eget ac eros. Morbi mi nisl, iaculis vel. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur.</p>
-        <p>Sed lorem leo, lobortis id nunc in, venenatis rhoncus leo. Fusce et tortor euismod, molestie metus at, placerat. </p>
+        <p>En esta sección puedes encontrar los contenidos que los niños aprenderán por etapa.</p>
+        <p>Ganémosle al Virus es una plataforma que forma parte de un programa de capacitación y apoyo para trabajar la prevención y autocuidado en niñas y niños durante la contingencia COVID-19,
+        donde se les permite a l@s niñ@s aprender del virus mientras avanzan por las etapas de los siguientes 3 módulos.</p>
+        <p>Los juegos incluidos en los módulos tienen sus propios detractores, como el personaje del coronavirus y el tiempo, y recompensas, es decir, puntos. </p>
+        <p>El material informativo en los módulos contará con un lenguaje adecuado para niños de 7 años en adelante, y dará recomendaciones y ejemplos con elementos conocidos y accesibles por ellos.</p>
       </div>
     </div>
   </article>
@@ -25,13 +28,13 @@ const ParaTutores = () => {
   <article className="episode">
     <div className="episode__number">01</div>
     <div className="episode__content">
-      <div className="title">Etapa 1</div>
+      <div className="title">Módulo 1</div>
       <div className="story">
-        <p>Nunc porta mi eu arcu pulvinar hendrerit. Sed et.</p>
-        <p>Quisque sed tortor vel nibh scelerisque blandit. Aliquam id libero semper, efficitur leo vel, egestas odio. Donec placerat malesuada lacus, a convallis odio sagittis. </p>
-        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer eget volutpat ipsum. Nunc facilisis laoreet fringilla. Phasellus nunc libero, placerat ut laoreet id, dictum a leo. Donec gravida quis eros vitae. </p>
-        <p>Sed vitae rhoncus augue. In pretium molestie nulla, ut tempor nibh mattis in. Nunc finibus ante a quam porttitor vestibulum. Nulla in libero risus. Mauris hendrerit porttitor fermentum. Donec rutrum, sapien. </p>
-        <p>Maecenas placerat a lorem at molestie. Donec placerat venenatis erat. Nullam auctor magna nec dolor accumsan porta. Duis eget luctus risus. Curabitur sollicitudin nulla ac magna convallis commodo. </p>
+        <p>Este primer módulo ofrece una explicación sobre la contingencia con un video animado de Hootie y el Escuadrón Prevención, personajes que previamente fueron presentados en el home de la página.</p>
+        <p>Luego, a través de una infografía con ilustraciones de los personajes se les explica a l@s niñ@s qué es el COVID-19, cuáles son los síntomas, cómo se da el contagio y cómo evitarlo.</p>
+        <p>Por último, l@s niñ@s aplican el conocimiento aprendido hasta el momento jugando un juego donde deben esquivar los virus que caen del cielo y atrapar los Jabier Jabones, homologando el sentido de que se debe rechazar el coronavirus y adoptar una cultura de lavarse las manos con jabón.</p>
+        <p>Vivimos algo histórico. Si hubiésemos hecho el ejercicio de imaginarnos lo que está ocurriendo antes de que pasara, probablemente no hubiésemos podido imaginar con exactitud todo lo que está ocurriendo y todo lo que esta situación conlleva.</p>
+        <p>Si para nosotros/as es difícil de dimensionar, ¿se imaginan lo que están percibiendo los niños y niñas del mundo?</p>
       </div>
     </div>
   </article>
@@ -39,15 +42,13 @@ const ParaTutores = () => {
   <article className="episode">
     <div className="episode__number">02</div>
     <div className="episode__content">
-      <div className="title">Etapa 2</div>
+      <div className="title">Módulo 2</div>
       <div className="story">
-        <p>Aliquam in tellus vulputate, venenatis sapien sit amet, pharetra purus. Praesent dapibus quam sed tempor dapibus. Maecenas quis lacus sagittis, maximus massa vel. </p>
-        <p>Aenean ut nibh ac metus fringilla lacinia eget ac eros. Morbi mi nisl, iaculis vel. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur.</p>
-        <p>Nunc porta mi eu arcu pulvinar hendrerit. Sed et.</p>
-        <p>Quisque sed tortor vel nibh scelerisque blandit. Aliquam id libero semper, efficitur leo vel, egestas odio. Donec placerat malesuada lacus, a convallis odio sagittis. </p>
-        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer eget volutpat ipsum. Nunc facilisis laoreet fringilla. Phasellus nunc libero, placerat ut laoreet id, dictum a leo. Donec gravida quis eros vitae. </p>
-        <p>Sed vitae rhoncus augue. In pretium molestie nulla, ut tempor nibh mattis in. Nunc finibus ante a quam porttitor vestibulum. Nulla in libero risus. Mauris hendrerit porttitor fermentum. Donec rutrum, sapien. </p>
-        <p>Maecenas placerat a lorem at molestie. Donec placerat venenatis erat. Nullam auctor magna nec dolor accumsan porta. Duis eget luctus risus. Curabitur sollicitudin nulla ac magna convallis commodo. </p>
+        <p>La psico-higiene es el tema principal del módulo 2, a veces olvidada por los adultos. Si para nosotros/as es difícil de dimensionar, ¿se imaginan lo que están percibiendo los niños y niñas del mundo?</p>
+        <p>Como veremos en este módulo, en cada etapa del desarrollo de un niño o niña, se dan distintos contextos de vinculación, que van moldeando distintos comportamientos. Y aquellos que se van instalando en su personalidad, pueden ser muy adecuados para el momento en que los aprenden, pero con el cambio de las circunstancias se pueden volver inadecuados, o derechamente perjudiciales.</p>
+        <p>El propósito del módulo, en todo caso, no es explorar conductas disfuncionales, sino aprender a distinguir las necesidades de aprendizaje de los distintos momentos de su desarrollo psicológico, para poder incorporar precauciones que protejan su salud psicológica, intelectual y afectiva, durante su proceso de socialización, en esta particular circunstancia de confinamiento.</p>
+        <p>Claramente, no existen criterios temporales rígidos, por lo que la evolución de los/as niños/as tiene cierta variación en los ritmos, especialmente en las primeras etapas y en lo referente al desarrollo de las capacidades motrices, lingüísticas y de autonomía personal.</p>
+        <p>Es indispensable propiciar acciones que estimulen el movimiento, porque eso tiene efectos positivos sobre nuestra salud física y mental –en el siguiente módulo se habla a cabalidad de esto. Utilicemos lo más posible el humor y documentemos por medio de fotos y videos familiares este momento que, no por atemorizante, deja de ser histórico.</p>
       </div>
     </div>
   </article>
@@ -55,22 +56,20 @@ const ParaTutores = () => {
   <article className="episode">
     <div className="episode__number">03</div>
     <div className="episode__content">
-      <div className="title">Etapa 3</div>
+      <div className="title">Módulo 3</div>
       <div className="story">
-      <p>Aliquam in tellus vulputate, venenatis sapien sit amet, pharetra purus. Praesent dapibus quam sed tempor dapibus. Maecenas quis lacus sagittis, maximus massa vel. </p>
-        <p>Aenean ut nibh ac metus fringilla lacinia eget ac eros. Morbi mi nisl, iaculis vel. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur.</p>
-        <p>Nunc porta mi eu arcu pulvinar hendrerit. Sed et.</p>
-        <p>Quisque sed tortor vel nibh scelerisque blandit. Aliquam id libero semper, efficitur leo vel, egestas odio. Donec placerat malesuada lacus, a convallis odio sagittis. </p>
-        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer eget volutpat ipsum. Nunc facilisis laoreet fringilla. Phasellus nunc libero, placerat ut laoreet id, dictum a leo. Donec gravida quis eros vitae. </p>
-        <p>Sed vitae rhoncus augue. In pretium molestie nulla, ut tempor nibh mattis in. Nunc finibus ante a quam porttitor vestibulum. Nulla in libero risus. Mauris hendrerit porttitor fermentum. Donec rutrum, sapien. </p>
-        <p>Maecenas placerat a lorem at molestie. Donec placerat venenatis erat. Nullam auctor magna nec dolor accumsan porta. Duis eget luctus risus. Curabitur sollicitudin nulla ac magna convallis commodo. </p>
-       <p>Mauris tempus, orci a tincidunt dignissim, ex ante gravida purus, eu gravida odio lacus non orci. In finibus magna sit amet lectus semper facilisis.</p>
-        <p>Nullam facilisis, arcu ut vulputate lacinia, justo dolor ullamcorper augue, ac dapibus erat orci id ligula. Curabitur venenatis auctor iaculis. Suspendisse potenti. Vivamus et augue ultricies, pharetra nulla pulvinar, fermentum nunc. Vestibulum nec erat. </p>
+      <p>Este módulo trata de la instalación y control de hábitos saludables para la inmunidad física y mental.</p>
+        <p>La instalación y control de hábitos tiene que ver con la manera en que las personas aprendemos.</p>
+        <p>Existen diversos modelos y tipos de aprendizajes. Cada cual es óptimo para distintos tipos de contenidos y etapas. No es lo mismo aprender a patear una pelota, que aprender la tabla del 13, o a regatear un precio; tampoco es lo mismo el aprendizaje que obtienes de leer un libro a los 6 años, que a los 16 o que a los 56.</p>
+        <p>Un niño o niña en confinamiento, requerirá instalar o modificar distintos tipos de hábitos, dependiendo de varios factores. Por esto, nos enfocaremos en el tipo de modelo que prima en el segmento de 7 a 10 años, en el sentido de que influirá en mayor tipo de conductas y hábitos a instalar.</p>
+        <p>A un niño o niña no se le puede enseñar a ser bueno u obediente, porque a los 7 años no han atravesado todavía las etapas del desarrollo para comprender lo conceptos de “bien”, “mal”, u “obediente”. A esa edad predomina la imitación, tanto de conductas como de conceptos; por lo tanto, un niño o niña puede repetir la frase “seré obediente”, o “me voy a portar bien”, pero para ellos sólo significa repetir conductas que le hacen parecer bueno y obediente.</p>
+        <p>Las conductas son acciones concretas, en circunstancias específicas. Enseñamos a los niños y niñas a realizar conductas acordes a ciertas circunstancias y a modificar su conducta cuando se produce en un contexto inadecuado. Por ejemplo, enseñamos a los infantes a demostrar afecto físico con su familia directa, pero no con extraños. O les enseñamos los beneficios de jugar, pero no a la hora de hacer la tarea. Como adultos, es fácil entender que la adecuación de una conducta es circunstancial, porque podemos abstraer y aplicar los conceptos de “circunstancia” y de “adecuación”.</p>
       </div>
     </div>
   </article>
 </div>
 </body>
+<Footer />
   </Fragment>
   )
 }
