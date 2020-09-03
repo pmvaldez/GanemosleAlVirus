286d7154edbf19a02c5058061619120bdf068bdf
pmvaldez <valdezp69@gmail.com>
2020-09-02 20:05:14 -0400

    css de download

--- virus/src/components/Download.jsx
+++ virus/src/components/Download.jsx
@@ -1,31 +1,58 @@
 import React, { Fragment } from 'react'
-import Footer from './Footer'
 import '../styles/download.css'
-import NavbarPage from './NavbarPage'
 import { MDBIcon } from "mdbreact";
-import { Link } from 'react-router-dom';
-import '../Files/pintaCovid.pdf'
+import covid from '../Files/pintaCovid.pdf';
+import guantrini from '../Files/pintaGuantrini.pdf';
+import hootie from '../Files/pintaHootie.pdf';
+import jabon from '../Files/pintaJABON.pdf';
+import puma from '../Files/pintaPuma.pdf';
+import tomas from '../Files/pintaTomask.pdf';
+import "../styles/levelOne.css";
+import Footer from './Footer'
+import logoGAV from '../img/GAV_logo.png';
+
 const Download = () => {
 
- 
-    return (
-    <Fragment>
-        <NavbarPage />
-        <div className="sectionDownload">
+return (
+<Fragment> 
+<div className="d-flex backgroundimg">
+  <div className="section-mod1-general">    
+  <div className="prog-bar-content d-flex pl-4 flex-wrap">
+      <div> <img src={logoGAV} className="logoGames" alt="..." /></div>
+    </div>
+    <div className="container text-center mt-4 mb-4">
+      <div className="p-4 whitebox">
+      <h1 className="contenido-titulo">Hola Amig@s</h1>
+      <div className="sectionDownload">
             <h1>Descarga y pinta tus personajes</h1> 
 
-            <a href="https://drive.google.com/file/d/16J-p-djFmyCggRusUSBpcCdEYaP60Np_/view?usp=sharing" download rel="noopener noreferrer" target="_blank" >
+            <a href={covid} download rel="noopener noreferrer" target="_blank" >
             <MDBIcon icon="bolt" size="3x" className="amber-text pr-3" />
             </a>      
-
-             
-            <Link path="../Files/pintaCovid.pdf" download target="_blank" rel="noopener noreferrer">
+            <a href={guantrini} download rel="noopener noreferrer" target="_blank" >
             <MDBIcon icon="bolt" size="3x" className="amber-text pr-3" />
-            </Link>       
+            </a>      
+            <a href={hootie} download rel="noopener noreferrer" target="_blank" >
+            <MDBIcon icon="bolt" size="3x" className="amber-text pr-3" />
+            </a>      
+            <a href={jabon} download rel="noopener noreferrer" target="_blank" >
+            <MDBIcon icon="bolt" size="3x" className="amber-text pr-3" />
+            </a>      
+            <a href={puma} download rel="noopener noreferrer" target="_blank" >
+            <MDBIcon icon="bolt" size="3x" className="amber-text pr-3" />
+            </a>      
+            <a href={tomas} download rel="noopener noreferrer" target="_blank" >
+            <MDBIcon icon="bolt" size="3x" className="amber-text pr-3" />
+            </a>      
+                  
 
         </div>
-        <Footer />
-    </Fragment>
+      </div>
+    </div>
+</div>        
+</div>
+<Footer />
+</Fragment> 
     )
 }
 
